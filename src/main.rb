79c49040c1eb116924465107cfdef20e0c54fd59
George Shore <george@georgeshore.com>
2016-02-13 22:07:11 +0000

    Update main.rb
    * Include all gameobjects and game states
    * Set search path for data assets

--- src/main.rb
+++ src/main.rb
@@ -1,10 +1,20 @@
 require 'gosu'
 require 'chingu'
 
+include Gosu
+
+ROOT_PATH = File.dirname(File.expand_path(__FILE__))
+DATA_PATH = File.join(ROOT_PATH, '..', 'data')
+
+Gosu::Image.autoload_dirs << File.join(DATA_PATH, 'images')
+
+require_all File.join(ROOT_PATH, 'gameobjects')
+require_all File.join(ROOT_PATH, 'gamestates')
+
 class Main < Chingu::Window
   def initialize
     super(800, 600)
-    self.input = { :escape => :close }
+    switch_game_state(PlayState)
   end
 
   def update
